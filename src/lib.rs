bcbff1e5b057536bc0c0be98f6db88ebbb3de512
LukK-Dev <luk@kaiser-gotha.de>
2024-03-19 00:38:24 +0100

    added background image

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod app;
+pub mod components;
 pub mod error_template;
 #[cfg(feature = "ssr")]
 pub mod fileserv;
+pub mod routes;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
