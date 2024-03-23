fb50ef7ea69224eef855a40a168dc4d6dc260bf0
richardisamu <133226713+richardisamu@users.noreply.github.com>
2024-03-23 11:51:27 +0100

    Update 19.03.

--- src/routes/homepage.rs
+++ src/routes/homepage.rs
@@ -5,7 +5,14 @@ use crate::components::navbar::Navbar;
 #[component]
 pub fn HomePage() -> impl IntoView {
     view! {
-        <Navbar/>
+        <div class="flex flex-col h-screen">
+            <Navbar/>
+            <div class="text-white h-screen w-full flex flex-col justify-center items-center">
+                <h1 class="font-semibold lg:text-8xl text-6xl mb-2">Arnoldi Schule</h1>
+                <span class="lg:text-5xl text-3xl">Staatliches Gymnasium</span>
+            </div>
+        </div>
+
         <div class="fixed inset-0 z-[-1] overflow-hidden">
             <img src="/school.jpg" class="object-center w-full h-full object-cover pointer-events-none brightness-50 blur-sm scale-105"/>
         </div>
